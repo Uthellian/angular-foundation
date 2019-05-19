af430ce098c3058dc88ec7d1598b4eb78fd47d30
Uthellian <mleong777@gmail.com>
2019-05-19 18:22:07 +1000

    Started working on dynamic forms

---
+++ src/app/shared/base/question-base.ts
@@ -0,0 +1,23 @@
+export class QuestionBase<T> {
+  value: T;
+  key: string
+  label: string;
+  order: number;
+  controlType: string;
+
+  constructor(
+    options: {
+      value?: T,
+      key?: string,
+      label?: string,
+      order?: number,
+      controlType?: string
+    } = {}
+  ) {
+      this.value = options.value;
+      this.key = options.key || '';
+      this.label = options.label || '';
+      this.order = options.order ? options.order : null;
+      this.controlType = options.controlType || '';
+  }
+}
